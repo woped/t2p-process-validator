674f107a5c3c7a19797d42d6f339fe879e4c78ef
nikolail45 <niko.ilincic@gmail.com>
2024-05-26 13:58:48 +0200

    added docker file, but disabled docker for now refactored

--- src/main/java/com/example/t2pvalidation/controller/XMLValidationController.java
+++ src/main/java/com/example/t2pvalidation/controller/XMLValidationController.java
@@ -1,4 +1,22 @@
 package com.example.t2pvalidation.controller;
 
+import com.example.t2pvalidation.service.XMLValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class XMLValidationController {
+
+    @Autowired
+    private XMLValidationService xmlValidationService;
+
+    @GetMapping("/xml_validate")
+    public String validateXml() {
+        String xmlFilePath = "src/main/resources/redhat_example.bpmn";
+        String xsdFilePath = "src/main/resources/BPMN20.xsd.xml";
+        boolean isValid = xmlValidationService.validateXml(xmlFilePath, xsdFilePath);
+        return isValid ? "XML is valid" : "XML is not valid";
+    }
 }
+
